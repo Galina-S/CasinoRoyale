41880799f951a3d27f1d5fac32f592d59be0e219
Galina S <98215274+Galina-S@users.noreply.github.com>
2022-07-19 21:58:08 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
 const kontostand = 150;
 let kontostand1 = kontostand;
-const prize= 35;
+const prize = 7000;
+
+const audio = new Audio("audio/button-20.mp3");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      audio.play();
+    });
+  });
+
+function generateDice (random, className) { 
+
+    switch (random) {
+        case 1: document.querySelector("."+className).classList = "fa-solid fa-dice-one fa-4x fa-beat" + " "+ className; break;
+        case 2: document.querySelector("."+className).classList = "fa-solid fa-dice-two fa-4x fa-beat" + " "+ className; break;
+        case 3: document.querySelector("."+className).classList = "fa-solid fa-dice-three fa-4x fa-beat"+ " " + className; break;
+        case 4: document.querySelector("."+className).classList = "fa-solid fa-dice-four fa-4x fa-beat" + " "+ className; break;
+        case 5: document.querySelector("."+className).classList = "fa-solid fa-dice-five fa-4x fa-beat" + " "+ className; break;
+        case 6: document.querySelector("."+className).classList = "fa-solid fa-dice-six fa-4x fa-beat"+ " " + className; break;
+    }
+    return random;
+}
 
 function run() {
     document.querySelector("#kontostand").innerHTML = kontostand1+"€";
-    let random1 = Math.round(Math.random()*5+1);
-    let random2 = Math.round(Math.random()*5+1);
-    let random3 = Math.round(Math.random()*5+1);
 
-    console.log(random1, random2, random3)
-    switch (random1) {
-        case 1: document.querySelector(".first").classList = "fa-solid first fa-dice-one fa-5x"; break;
-        case 2: document.querySelector(".first").classList = "fa-solid first fa-dice-two fa-5x"; break;
-        case 3: document.querySelector(".first").classList = "fa-solid first fa-dice-three fa-5x"; break;
-        case 4: document.querySelector(".first").classList = "fa-solid first fa-dice-four fa-5x"; break;
-        case 5: document.querySelector(".first").classList = "fa-solid first fa-dice-five fa-5x"; break;
-        case 6: document.querySelector(".first").classList = "fa-solid first fa-dice-six fa-5x"; break;
-    }
+    let random1 = generateDice(Math.round(Math.random()*5+1), "first");
+    let random2 = generateDice(Math.round(Math.random()*5+1), "second");
+    let random3 = generateDice(Math.round(Math.random()*5+1), "third");
+    let random4 = generateDice(Math.round(Math.random()*5+1), "fourth");
+    let random5 = generateDice(Math.round(Math.random()*5+1), "fifth");
+    let random6 = generateDice(Math.round(Math.random()*5+1), "sixth");
 
-    switch (random2) {
-        case 1: document.querySelector(".second").classList = "fa-solid second fa-dice-one fa-5x"; break;
-        case 2: document.querySelector(".second").classList = "fa-solid second fa-dice-two fa-5x"; break;
-        case 3: document.querySelector(".second").classList = "fa-solid second fa-dice-three fa-5x"; break;
-        case 4: document.querySelector(".second").classList = "fa-solid second fa-dice-four fa-5x"; break;
-        case 5: document.querySelector(".second").classList = "fa-solid second fa-dice-five fa-5x"; break;
-        case 6: document.querySelector(".second").classList = "fa-solid second fa-dice-six fa-5x"; break;
-    }
-
-    switch (random3) {
-        case 1: document.querySelector(".third").classList = "fa-solid third fa-dice-one fa-5x"; break;
-        case 2: document.querySelector(".third").classList = "fa-solid third fa-dice-two fa-5x"; break;
-        case 3: document.querySelector(".third").classList = "fa-solid third fa-dice-three fa-5x"; break;
-        case 4: document.querySelector(".third").classList = "fa-solid third fa-dice-four fa-5x"; break;
-        case 5: document.querySelector(".third").classList = "fa-solid third fa-dice-five fa-5x"; break;
-        case 6: document.querySelector(".third").classList = "fa-solid third fa-dice-six fa-5x"; break;
-    }
-   
-    if ((random1 == random2)&&(random1 == random3)) {
-        kontostand1+=prize;
-        document.querySelector("#prize").innerHTML ="Du bist ein Glückspilz! +" + prize+"€";
+    console.log(random1, random2, random3, random4, random5, random6);
+    
+    if ((random1 == random2)&&(random1 == random3)&&(random1 == random4)&&(random1 == random5)&&(random1 == random6)) {
+    kontostand1+=prize;
     } else {
         kontostand1-=1;
-        document.querySelector("#prize").innerHTML ="";
-    }
-}
+    };
+};
 
 function revert() {
     document.querySelector("#kontostand").innerHTML = kontostand+"€";
-    document.querySelector("#prize").innerHTML ="";
-}
+    kontostand1 = kontostand;
+};
+
