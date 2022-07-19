92f966f9877b520e257c40d56325648b8c2eb689
Galina S <98215274+Galina-S@users.noreply.github.com>
2022-07-19 10:45:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-let kontostand = 100;
+const kontostand = 150;
+let kontostand1 = kontostand;
 
 function run() {
-    document.querySelector("#kontostand").innerHTML = kontostand+"€";
+    document.querySelector("#kontostand").innerHTML = kontostand1+"€";
     let random = Math.round(Math.random()*5+1);
-    document.querySelector("#randomNumber").innerHTML = random;
+    console.log(random);
+    //document.querySelector("#randomNumber").innerHTML = random;
+
+    switch (random) {
+        case 1: document.querySelector(".fa-solid").classList = "fa-solid fa-dice-one fa-5x"; break;
+        case 2: document.querySelector(".fa-solid").classList = "fa-solid fa-dice-two fa-5x"; break;
+        case 3: document.querySelector(".fa-solid").classList = "fa-solid fa-dice-three fa-5x"; break;
+        case 4: document.querySelector(".fa-solid").classList = "fa-solid fa-dice-four fa-5x"; break;
+        case 5: document.querySelector(".fa-solid").classList = "fa-solid fa-dice-five fa-5x"; break;
+        case 6: document.querySelector(".fa-solid").classList = "fa-solid fa-dice-six fa-5x"; break;
+    }
+
     if (random == 6) {
-        kontostand+=5;
+        kontostand1+=5;
     } else {
-        kontostand-=1;
+        kontostand1-=1;
     }
 }
 
 function revert() {
-    document.querySelector("#kontostand").innerHTML = 100+"€";
+    document.querySelector("#kontostand").innerHTML = kontostand+"€";
 }
